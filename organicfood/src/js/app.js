5679a093557cd7887124312592cbce8b54b83bf6
kelvin <grishakelvin@gmail.com>
2020-07-18 02:04:24 +0300

    organic food v.1.1 fix DRY

--- organicfood/src/js/app.js
+++ organicfood/src/js/app.js
@@ -1,3 +1,5 @@
+//Для кнопки в header
+
 const headerButton = document.querySelector(".of-header__button");
 let clickCounter = 0;
 headerButton.addEventListener('click', (event) => {
@@ -9,3 +11,21 @@ headerButton.addEventListener('click', (event) => {
 headerButton.addEventListener('focus', (e) => {
 	e.target.style.border = 'none'; 
 })
+
+//Для фона в products
+
+const k = 1;
+let g;
+
+const productCards = document.querySelectorAll(".product-item");
+console.log(productCards.length);
+for (let i = 0; i < productCards.length; i++) {
+	g = i+1;
+	if (i === 3) {
+		productCards[i].style.background = "none";
+	} else {
+		productCards[i].style.backgroundImage = "url('./img/img" + g + ".jpg')"; 
+	}
+}
+
+//awesome анимация 
