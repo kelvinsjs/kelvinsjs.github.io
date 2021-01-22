b5eff037cf03b9dc024d1177e78d82c616e8ce07
Kelvin <grishakelvin@gmail.com>
2021-01-22 23:06:35 +0200

    update 22/01 2-nd semester

--- passwordgenerator/app.js
+++ passwordgenerator/app.js
@@ -4,14 +4,16 @@ const getImageWithEye = document.querySelector(".img-icon");
 const getImageBlock = document.querySelector(".password-watch");
 const getPasswordBlock = document.querySelector(".password-result");
 const mainButton = document.querySelector(".generate-button");
+const copyButton = document.querySelector(".copy-button");
 const getCheckboxes = document.querySelectorAll(".settings-checkbox");
 const getSymbolsCount = document.querySelector(".settings-number");
-
-console.log(getCheckboxes);
-
+const getSettingsList = document.querySelector(".settings-list");
+const getControlSpan = document.getElementById("control-span");
+const randomString = "string";
 let passwordText = getPasswordBlock.innerText;
 
 getImageBlock.addEventListener('click', () => {
+
   if (getImageWithEye.getAttribute('src') == "eye.png") {
     getImageWithEye.src = "eye-slash.png";
     getPasswordBlock.innerText = "************";
@@ -49,7 +51,9 @@ function generatePassword() {
     currentArray = currentArray.concat(specialSymbolsArray);
   }
   if (getCheckboxes[4].checked == false) {
-    currentArray = currentArray.concat(hardReadSymbolsArray);
+    if (getCheckboxes[0].checked || getCheckboxes[1].checked) {
+      currentArray = currentArray.concat(hardReadSymbolsArray);
+    }
   }
   for (let i = 0; i < getSymbolsCount.value; i++) {
     let thisRandomNumber = Math.floor(Math.random() * Math.floor(currentArray.length));
@@ -62,6 +66,33 @@ function generatePassword() {
     getPasswordBlock.innerText = passwordText;
     getImageWithEye.src = "eye.png";
   }
+  getControlSpan.innerText = passwordText;
 }
 
 mainButton.addEventListener('click', generatePassword);
+
+getCheckboxes[5].addEventListener("change", () => {
+  localStorage.setItem('immediately', getCheckboxes[5].checked);
+})
+
+
+// function checkLocalStorage(checkingItem) {
+//   return localStorage.getItem(`$(checkingItem)`);
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(`'`.concat(randomString, "'"));
+})
+
+// copyButton.onclick = function() {
+//
+//   copyText.select();
+//   document.execCommand("copy");
+// }
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+}
+
+copyButton.addEventListener("click", () => {
+  copyText(passwordText);
+})
