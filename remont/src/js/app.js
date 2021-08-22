828deb0a0e774254cd05f26aec9da64ef9ace9da
Kelvin <grishakelvin@gmail.com>
2021-08-23 01:10:39 +0300

    remont v.0.3. add modal window + full responsive + array rewriting

--- remont/src/js/app.js
+++ remont/src/js/app.js
@@ -2,6 +2,14 @@ let formFiller = [
     {
         value: "Тип роботи",
         desc: ""
+    },
+    {
+        value: "Ім'я",
+        desc: ""
+    },
+    {
+        value: "Телефон",
+        desc: ""
     }
 ];
 
@@ -34,4 +42,24 @@ hambButton.addEventListener("click", (e) => {
     e.preventDefault();
 document.querySelector(".re-header__list").classList.toggle("invisible");
 document.querySelector(".fa-bars").classList.toggle("fa-times");
+})
+
+function closeModal() {
+    document.querySelector(".re-modal").classList.toggle("so-invisible");
+}
+
+const callButton = document.querySelector(".re-header__callButton");
+callButton.addEventListener("click", closeModal);
+
+const closeFormButton = document.querySelector(".close-form");
+closeFormButton.addEventListener("click", closeModal);
+
+const getFormSubmitButton = document.querySelector(".re-modal__form");
+const a = document.querySelectorAll('[name="inputField"]');
+getFormSubmitButton.addEventListener("submit", (e) => {
+    e.preventDefault();
+    for(let i = 0; i < a.length; i++) {
+        changeDesc(formFiller[(i*1)+1].value, a[i].value);
+    }
+    console.log(formFiller)
 })
